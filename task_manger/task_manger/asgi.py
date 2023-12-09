ce6d7fb74c3f5613a89dbe577ca2d36e6531d369
Irisha <irisha.kostevich@mail.ru>
2023-12-09 13:32:15 +0300

    Refactoring

--- task_manger/task_manger/asgi.py
+++ task_manger/task_manger/asgi.py
@@ -1,15 +1,13 @@
-"""
-ASGI config for task_manger project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
+from django.core.asgi import get_asgi_application
+
 
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manger.settings')
 
--- task_manger/task_manger/wsgi.py
+++ task_manger/task_manger/wsgi.py
@@ -1,15 +1,12 @@
-"""
-WSGI config for task_manger project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
-import os
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
 
 from django.core.wsgi import get_wsgi_application
+
+
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manger.settings')
 
